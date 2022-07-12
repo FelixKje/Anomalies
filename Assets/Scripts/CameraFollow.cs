a7b2b2569c0077b66864c4c35ee14574d750b28f
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-07-12 16:15:00 +0200

    Added a camera follow scripts that can follow different targets

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField] float cameraMoveSpeed =2f;
+    Func<Vector3> GetCameraFollowPositionFunc;
+
+    public void Setup(Func<Vector3> GetCameraFollowPositionFunc) {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+    }
+
+    public void SetGetCameraFollowPositionFunc(Func<Vector3> GetCameraFollowPositionFunc) {
+        this.GetCameraFollowPositionFunc = this.GetCameraFollowPositionFunc;
+    }
+
+    void Update() {
+        Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
+        cameraFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+
+        if (distance > 0) {
+            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+        }
+    }
+}
