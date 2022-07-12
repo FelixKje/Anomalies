f67325d6f91f990506394127fdebf18b36a3c49e
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-07-12 13:25:16 +0200

    Added a Weapon aim that aims at the mouse position

---
+++ Assets/Scripts/Movement/PlayerAim.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Utility;
+
+namespace Movement {
+    public class PlayerAim : MonoBehaviour {
+        [SerializeField] Transform weapon;
+        void Update() {
+            Vector3 mousePosition = Utilities.GetMouseWorldPosition();
+            Vector3 aimDirection = (mousePosition - transform.position).normalized;
+
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+            weapon.transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+}
