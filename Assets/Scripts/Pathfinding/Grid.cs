e9ed402147e099b1338521c965b63178130008fc
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-07-14 17:43:18 +0200

    Worked on grid

--- Assets/Scripts/Pathfinding/Grid.cs
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -6,6 +6,7 @@ namespace Pathfinding {
         int height;
         float cellSize;
         int[,] gridArray;
+        TextMesh[,] debugTextArray;
 
         public Grid(int width, int height, float cellSize) {
             this.width = width;
@@ -13,20 +14,29 @@ namespace Pathfinding {
             this.cellSize = cellSize;
 
             gridArray = new int[width, height];
+            debugTextArray = new TextMesh[width, height];
 
             for (int x = 0; x < gridArray.GetLength(0); x++) {
                 for (int y = 0; y < gridArray.GetLength(1); y++) {
-                    Utilities.CreateWorldText(gridArray[x, y].ToString(), null,GetWorldPosition(x,y) + new Vector3(cellSize,cellSize) * .5f, 10, Color.white, TextAnchor.MiddleCenter);
+                    debugTextArray[x,y] = Utilities.CreateWorldText(gridArray[x, y].ToString(), null,GetWorldPosition(x,y) + new Vector3(cellSize,cellSize) * .5f, 10, Color.white, TextAnchor.MiddleCenter);
                     Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x,y +1), Color.black, 100f);
                     Debug.DrawLine(GetWorldPosition(x,y), GetWorldPosition(x + 1,y), Color.black, 100f);
                 }
             }
             Debug.DrawLine(GetWorldPosition(0,height), GetWorldPosition(width,height), Color.black, 100f);
             Debug.DrawLine(GetWorldPosition(width,0), GetWorldPosition(width,height), Color.black, 100f);
+            
+            SetValue(2,1, 56);
         }
 
         Vector3 GetWorldPosition(int x, int y) {
             return new Vector3(x, y) * cellSize;
+        }
+        public void SetValue(int x, int y, int value) {
+            if (x >= 0 && y >= 0 && x < width && y < height) {
+                gridArray[x, y] = value;
+                debugTextArray[x, y].text = gridArray[x,y].ToString();
+            }
         }
     }
 }
