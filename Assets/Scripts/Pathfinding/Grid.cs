caaa979806afc072d237a4675613acdec03624c1
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-07-14 15:15:01 +0200

    Basic grid class

---
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -0,0 +1,14 @@
+namespace Pathfinding {
+    public class Grid {
+        int width;
+        int height;
+        int[,] gridArray;
+
+        public Grid(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            gridArray = new int[width, height];
+        }
+    }
+}
