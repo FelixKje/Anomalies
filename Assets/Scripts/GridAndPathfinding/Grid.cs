fd28d4f5b017547230e1dbd433fdf934b73e2c79
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-07-25 19:47:15 +0200

    Can now create grid of any type

--- Assets/Scripts/GridAndPathfinding/Grid.cs
+++ Assets/Scripts/GridAndPathfinding/Grid.cs
@@ -1,20 +1,33 @@
+using System;
 using UnityEngine;
 using Utility;
-public class Grid {
+public class Grid<TGridObject> {
+
+    public event EventHandler<GridObjectChangedEventArgs> GridObjectChanged;
+    public class GridObjectChangedEventArgs : EventArgs {
+        public int x;
+        public int y;
+    }
     int width;
     int height;
     float cellSize;
     Vector3 originPosition;
-    int[,] gridArray;
+    TGridObject[,] gridArray;
     TextMesh[,] debugTextArray;
 
-    public Grid(int width, int height, float cellSize, Vector3 originPosition) {
+    public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject) {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
 
-        gridArray = new int[width, height];
+        gridArray = new TGridObject[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++) {
+            for (int y = 0; y < gridArray.GetLength(1); y++) {
+                gridArray[x, y] = createGridObject(this,x,y);
+            }
+        }
         debugTextArray = new TextMesh[width, height];
 
     for (int x = 0; x < gridArray.GetLength(0); x++) {
@@ -26,8 +39,6 @@ public class Grid {
         }
         Debug.DrawLine(GetWorldPosition(0,height), GetWorldPosition(width,height), Color.black, 100f);
         Debug.DrawLine(GetWorldPosition(width,0), GetWorldPosition(width,height), Color.black, 100f);
-        
-        SetValue(2,1, 56);
     }
 
     Vector3 GetWorldPosition(int x, int y) {
@@ -37,31 +48,35 @@ public class Grid {
         x = Mathf.FloorToInt((worldPosition - originPosition).x/cellSize);
         y = Mathf.FloorToInt((worldPosition - originPosition).y/cellSize);
     }
-    public void SetValue(int x, int y, int value) {
+    public void SetGridObject(int x, int y, TGridObject value) {
         if (x >= 0 && y >= 0 && x < width && y < height) {
             gridArray[x, y] = value;
-            debugTextArray[x, y].text = gridArray[x,y].ToString();
+            if (GridObjectChanged != null) GridObjectChanged(this, new GridObjectChangedEventArgs { x = x, y = y });
         }
     }
+
+    public void TriggerGridObjectChanged(int x, int y) {
+        if (GridObjectChanged != null) GridObjectChanged(this, new GridObjectChangedEventArgs { x = x, y = y });
+    }
     
-    public void SetValue(Vector3 worldPosition, int value) {
+    public void SetGridObject(Vector3 worldPosition, TGridObject value) {
         int x, y;
         GetXY(worldPosition, out x, out y);
-        SetValue(x,y,value);
+        SetGridObject(x,y,value);
     }
 
-    public int GetValue(int x, int y) {
+    public TGridObject GetGridObject(int x, int y) {
         if (x >= 0 && y >= 0 && x < width && y < height) {
             return gridArray[x, y];
         }
         else {
-            return 0;
+            return default(TGridObject);
         }
     }
 
-    public int GetValue(Vector3 worldPosition) {
+    public TGridObject GetGridObject(Vector3 worldPosition) {
         int x, y;
         GetXY(worldPosition, out x, out y);
-        return GetValue(x, y);
+        return GetGridObject(x, y);
     }
 }
--- Assets/Scripts/GridAndPathfinding/Testgrid.cs
+++ Assets/Scripts/GridAndPathfinding/Testgrid.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 using Utility;
 public class Testgrid : MonoBehaviour {
     Grid<bool> grid;
-    void Start() { 
-        grid = new Grid<bool>(4, 2, 5f, new Vector3(-2 * 5f,-1 * 5f));
-    }
-
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            grid.SetValue(Utilities.GetMouseWorldPosition(), true);
-        }
-        if (Input.GetMouseButtonDown(1)) {
-            Debug.Log(grid.GetValue(Utilities.GetMouseWorldPosition()));
-        }
-    }
+    // void Start() { 
+    //     grid = new Grid<bool>(4, 2, 5f, new Vector3(-2 * 5f,-1 * 5f));
+    // }
+    //
+    // void Update() {
+    //     if (Input.GetMouseButtonDown(0)) {
+    //         grid.SetGridObject(Utilities.GetMouseWorldPosition(), true);
+    //     }
+    //     if (Input.GetMouseButtonDown(1)) {
+    //         Debug.Log(grid.GetGridObject(Utilities.GetMouseWorldPosition()));
+    //     }
+    // }
 }
