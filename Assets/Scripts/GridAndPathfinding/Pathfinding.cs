0bc0b4413fe3660a9d223487e5005d78aba33b15
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-08-01 13:52:32 +0200

    Created script for PathNodes

--- Assets/Scripts/GridAndPathfinding/Pathfinding.cs
+++ Assets/Scripts/GridAndPathfinding/Pathfinding.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pathfinding 
-{
+public class Pathfinding {
+    Grid<PathNode> grid;
     public Pathfinding(int width, int height) {
-        
+        grid = new Grid<PathNode>(width, height, 10f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
     }
 }
