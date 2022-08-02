e1291cdf0592efcd44886d3c8ce92604756859fb
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-08-02 19:55:39 +0200

    RTS Controller can now command units to move to nodes on the grid

--- Assets/Scripts/RTS/GameRTSController.cs
+++ Assets/Scripts/RTS/GameRTSController.cs
@@ -17,6 +17,20 @@ public class GameRTSController : MonoBehaviour {
         selectionAreaTransform.gameObject.SetActive(false);
     }
     void Update() {
+        HandleSelectingUnits();
+        HandleCommandingSelectedUnits();
+    }
+
+    void HandleCommandingSelectedUnits() {
+        if (Input.GetMouseButtonDown(1)) {
+            Vector3 moveToPosition = Utilities.GetMouseWorldPosition();
+            foreach (var unit in selectedUnitRTSList) {
+                unit.MoveTo(moveToPosition);
+            }
+        }
+    }
+
+    void HandleSelectingUnits() {
         if (Input.GetMouseButtonDown(0)) {
             selectionAreaTransform.gameObject.SetActive(true);
             startPosition = Utilities.GetMouseWorldPosition();
@@ -27,12 +41,12 @@ public class GameRTSController : MonoBehaviour {
             Vector3 lowerLeft = new Vector3(
                 Mathf.Min(startPosition.x, currentMousePosition.x),
                 Mathf.Min(startPosition.y, currentMousePosition.y)
-                );
+            );
             
             Vector3 upperRight = new Vector3(
                 Mathf.Max(startPosition.x, currentMousePosition.x),
                 Mathf.Max(startPosition.y, currentMousePosition.y)
-                );
+            );
             selectionAreaTransform.position = lowerLeft;
             selectionAreaTransform.localScale = upperRight - lowerLeft;
         }
--- Assets/Scripts/RTS/UnitRTS.cs
+++ Assets/Scripts/RTS/UnitRTS.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 public class UnitRTS : MonoBehaviour {
 
     GameObject selectedGameObject;
+    IMovePosition movePosition;
 
     void Awake() {
         selectedGameObject = transform.Find("Selected").gameObject;
+        movePosition = GetComponent<IMovePosition>();
         SetSelectedVisible(false);
     }
 
     public void SetSelectedVisible(bool visible) {
         selectedGameObject.SetActive(visible);
+    }
+
+    public void MoveTo(Vector3 targetPosition) {
+        movePosition.SetMovePosition(targetPosition);
     }
 }
