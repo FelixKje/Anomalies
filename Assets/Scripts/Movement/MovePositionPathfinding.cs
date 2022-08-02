9a615a1fb50f65a8f8c27c2d4cc8379fdc2f4844
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-08-02 19:54:29 +0200

    Added modular movement to work with pathfinding

---
+++ Assets/Scripts/Movement/MovePositionPathfinding.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionPathfinding : MonoBehaviour, IMovePosition {
+    
+    List<Vector3> pathVectorList;
+    int pathIndex = -1;
+
+    public void SetMovePosition(Vector3 movePosition) {
+        Debug.Log("Trying to find path");
+        pathVectorList = Pathfinding.Instance.FindPath(transform.position, movePosition);
+        if (pathVectorList.Count > 0) {
+            pathIndex = 0;
+            foreach (var pathNode in pathVectorList) {
+                Debug.Log(pathNode);
+            }
+            Debug.Log(pathVectorList.Count);
+        }
+    }
+
+    void Update() {
+        if (pathIndex != -1) {
+            Vector3 nextPathPosition = pathVectorList[pathIndex];
+            Vector3 moveVelocity = (nextPathPosition - transform.position).normalized;
+            GetComponent<IMoveVelocity>().SetVelocity(moveVelocity);
+
+            float reachedPositionDistance = 6f;
+            Debug.Log(Vector3.Distance(transform.position, nextPathPosition));
+            if (Vector3.Distance(transform.position, nextPathPosition) < reachedPositionDistance) {
+                Debug.Log("Node reached next index:" + pathIndex);
+                pathIndex++;
+                if (pathIndex >= pathVectorList.Count) {
+                    pathIndex = -1;
+                }
+            }
+        } else {
+            GetComponent<IMoveVelocity>().SetVelocity(Vector3.zero);
+        }
+        
+    }
+
+}
