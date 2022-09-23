6df024e6d8e54082d2d2b85cf38609179f88f013
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-09-23 19:57:02 +0200

    Shooting and stuff

--- Assets/Scripts/Combat/PlayerAim.cs
+++ Assets/Scripts/Combat/PlayerAim.cs
@@ -1,26 +1,35 @@
+using Unity.Mathematics;
 using UnityEngine;
 using Utility;
+using Random = UnityEngine.Random;
 
 namespace Movement {
     public class PlayerAim : MonoBehaviour {
-        [SerializeField] Transform weapon;
+        [SerializeField] Transform bulletSpawnPoint;
         [SerializeField][Range(0f,90f)] float bulletSpread = 0.05f;
+        [SerializeField] GameObject bulletGameObject;
+
+        bool isDebug;
         void Update() {
             Aim();
             Shoot();
         }
         void Shoot() {
-            if (Input.GetMouseButtonDown(0)) {
+            if (Input.GetMouseButton(0)) {
+                Instantiate(bulletGameObject, bulletSpawnPoint);
+                
                 var mousePosition = Utilities.GetMouseWorldPosition();
                 var direction = (mousePosition - transform.position).normalized;
-                
-                var randomHitPosX = Random.Range(mousePosition.x - bulletSpread, mousePosition.x + bulletSpread);
-                var randomHitPosY = Random.Range(mousePosition.y - bulletSpread, mousePosition.y + bulletSpread);
-                var randomHitPos = new Vector3(randomHitPosX, randomHitPosY, direction.z);
-                
-                Debug.DrawLine(weapon.position, randomHitPos, Color.red, 1f);
-                Debug.Log(direction);
 
+                isDebug = false;
+                if (isDebug) {
+                    var randomHitPosX = Random.Range(mousePosition.x - bulletSpread, mousePosition.x + bulletSpread);
+                    var randomHitPosY = Random.Range(mousePosition.y - bulletSpread, mousePosition.y + bulletSpread);
+                    var randomHitPos = new Vector3(randomHitPosX, randomHitPosY, direction.z);
+                
+                    Debug.DrawLine(bulletSpawnPoint.position, randomHitPos, Color.red, 1f);
+                    Debug.Log(direction);
+                }
             }
         }
         void Aim() {
@@ -29,7 +38,7 @@ namespace Movement {
 
             float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
 
-            weapon.transform.eulerAngles = new Vector3(0, 0, angle);
+            bulletSpawnPoint.transform.eulerAngles = new Vector3(0, 0, angle);
         }
     }
 }
